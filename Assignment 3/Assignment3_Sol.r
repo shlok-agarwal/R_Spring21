414ff14213acca6aaadceb82a35c463ae9695440
shlok <ssagarwal@wpi.edu>
2021-02-17 11:22:30 -0500

    write in the function format

--- Assignment 3/Assignment3_Sol.r
+++ Assignment 3/Assignment3_Sol.r
@@ -1,4 +1,3 @@
-
 ## Import libraries
 library(dplyr)
 library(stringr)
@@ -8,17 +7,24 @@ library(tidyr)
 library(reshape2)
 library(ggplot2)
 
-## Import data
-callData001 <- read.csv(file.choose(), header = T)
-SCD001 <- as.data.frame(callData001 %>%  dplyr::summarize( 
+# ## Import data
+setwd("C:/R_ws/R_Spring21/Assignment 3")
+callMeans <- function(callTypes, id){
+    ## Check for valid input
+    ## Read all .csv files associated with id numbers included in the id argument
+    id = sprintf("%03d", id)
+    path <- paste0(paste0("callData/",id), ".csv")
+    CD_ID <- read.csv(path, header = TRUE)
+    SCD001 <- as.data.frame(CD_ID %>%  dplyr::summarize( 
                                                       mean = mean(totalCalls, na.rm = TRUE),
                                                       sd = sd(totalCalls, na.rm = TRUE),
                                                       max = max(totalCalls, na.rm = TRUE),
                                                       min = min(totalCalls, na.rm = TRUE))
                                  )
 
-result <- data.frame(callTypes = 'totalCalls', mean = SCD001$mean, sd = SCD001$sd, max = SCD001$max, min = SCD001$min)
+    result <- data.frame(callTypes = 'totalCalls', mean = SCD001$mean, sd = SCD001$sd, max = SCD001$max, min = SCD001$min)
+    return(result)
+}
 
-print(statscallData001)
-
-## Clean data
+a <- callMeans(callTypes = "totalCalls", id = 002)
+print(a)
