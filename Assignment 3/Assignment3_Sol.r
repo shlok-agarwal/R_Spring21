e25375a48d8f8383e63ece1ba1041ef41ef97f39
shlok <ssagarwal@wpi.edu>
2021-02-17 10:35:18 -0500

    get frame in desired format

--- Assignment 3/Assignment3_Sol.r
+++ Assignment 3/Assignment3_Sol.r
@@ -10,12 +10,14 @@ library(ggplot2)
 
 ## Import data
 callData001 <- read.csv(file.choose(), header = T)
-statscallData001 <- as.data.frame(callData001 %>%  dplyr::summarize( 
+SCD001 <- as.data.frame(callData001 %>%  dplyr::summarize( 
                                                       mean = mean(totalCalls, na.rm = TRUE),
                                                       sd = sd(totalCalls, na.rm = TRUE),
                                                       max = max(totalCalls, na.rm = TRUE),
                                                       min = min(totalCalls, na.rm = TRUE))
                                  )
+
+result <- data.frame(callTypes = 'totalCalls', mean = SCD001$mean, sd = SCD001$sd, max = SCD001$max, min = SCD001$min)
 
 print(statscallData001)
 
